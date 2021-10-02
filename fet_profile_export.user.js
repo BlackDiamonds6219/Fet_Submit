0d1708a284ac86bd99c720dd03ce86ce5de38e92
BlackDiamonds <blackdiamonds6219@gmail.com>
2021-10-02 13:19:27 -0400

    added explicit url of google script to @connect

--- fet_profile_export.user.js
+++ fet_profile_export.user.js
@@ -13,7 +13,7 @@
 // @exclude        https://fetlife.com/chat/*
 // @exclude        https://fetlife.com/im_sessions*
 // @exclude        https://fetlife.com/polling/*
-// @connect        https://script.google.com/*
+// @connect        https://script.google.com/macros/s/AKfycbxOTFhjbVvzPLnTdv6ATLkwn8QtGW1XNUmWzSTdnRLl/dev
 // @grant          GM_log
 // @grant          GM_xmlhttpRequest
 // @grant          GM_addStyle
